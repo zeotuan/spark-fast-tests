c351121e640a3a4ed1fb2f18dc90c8adf995f6c0
Tuan Pham <Tuan.Pham@wisetechglobal.com>
2024-04-29 18:17:18 +1000

    Support less strict comparison between Number

--- src/main/scala/com/github/mrpowers/spark/fast/tests/RowComparer.scala
+++ src/main/scala/com/github/mrpowers/spark/fast/tests/RowComparer.scala
@@ -1,15 +1,13 @@
 package com.github.mrpowers.spark.fast.tests
 
 import org.apache.commons.math3.util.Precision
-
-import scala.math.abs
 import org.apache.spark.sql.Row
 
 object RowComparer {
 
   /** Approximate equality, based on equals from [[Row]] */
-  def areRowsEqual(r1: Row, r2: Row, tol: Double = 0, strictType: Boolean = true): Boolean = {
-    if (tol == 0 && strictType) {
+  def areRowsEqual(r1: Row, r2: Row, tol: Double = 0): Boolean = {
+    if (tol == 0) {
       return r1 == r2
     }
     if (r1.length != r2.length) {
@@ -24,15 +22,17 @@ object RowComparer {
         val o2 = r2.get(i)
         val valid = o1 match {
           case b1: Array[Byte] =>
-            (o2.isInstanceOf[Array[Byte]] || !strictType) && java.util.Arrays.equals(b1, o2.asInstanceOf[Array[Byte]])
-          case f1: Float => (o2.isInstanceOf[Float] || !strictType) &&
-            Precision.equalsIncludingNaN(f1, o2.asInstanceOf[Float], tol)
-          case d1: Double =>
-            (o2.isInstanceOf[Double] || !strictType) &&
-              Precision.equalsIncludingNaN(d1, o2.asInstanceOf[Double], tol)
-          case bd1: java.math.BigDecimal if o2.isInstanceOf[java.math.BigDecimal] || !strictType =>
+            o2.isInstanceOf[Array[Byte]] && java.util.Arrays.equals(b1, o2.asInstanceOf[Array[Byte]])
+          case f1: Float if o2.isInstanceOf[Float]   => Precision.equalsIncludingNaN(f1, o2.asInstanceOf[Float], tol)
+          case d1: Double if o2.isInstanceOf[Double] => Precision.equalsIncludingNaN(d1, o2.asInstanceOf[Double], tol)
+          case bd1: java.math.BigDecimal if o2.isInstanceOf[java.math.BigDecimal] =>
             bd1.subtract(o2.asInstanceOf[java.math.BigDecimal]).abs().compareTo(new java.math.BigDecimal(tol)) == -1
-          case _ => o1 == o2
+          case f1: Number if o2.isInstanceOf[Number] =>
+            val bd1 = new java.math.BigDecimal(f1.toString)
+            val bd2 = new java.math.BigDecimal(o2.toString)
+            bd1.subtract(bd2).abs().compareTo(new java.math.BigDecimal(tol)) == -1
+          case rr1: Row if o2.isInstanceOf[Row] => areRowsEqual(rr1, o2.asInstanceOf[Row], tol)
+          case _                                => o1 == o2
         }
         if (!valid) {
           return false
